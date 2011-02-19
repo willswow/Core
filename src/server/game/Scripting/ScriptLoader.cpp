37a44d675bd210533d381062b0fc082ab32afcb6
Will <willish@willswow.com>
2011-02-19 03:20:29 +0000

    Installed Passive Anticheat system.
    This system will not boot cheaters automatically from the game, instead it reports all abnormal behaviour. Individuals reporting higher ammounts of abnormal bahaviour or 'reports' will be investigated by a Gamemaster. These investigations will be invisible to the general playerbase.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -48,6 +48,7 @@ void AddSC_SmartSCripts();
 void AddSC_Npc_Beastmaster();
 
 //Commands
+void AddSC_anticheat_commandscript();
 void AddSC_account_commandscript();
 void AddSC_achievement_commandscript();
 void AddSC_debug_commandscript();
@@ -644,6 +645,7 @@ void AddSpellScripts()
 
 void AddCommandScripts()
 {
+    AddSC_anticheat_commandscript();
     AddSC_account_commandscript();
     AddSC_achievement_commandscript();
     AddSC_debug_commandscript();
