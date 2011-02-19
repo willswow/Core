992f66c0b85b5eb86efd2801aa541017f3a2883d
Will <willish@willswow.com>
2011-02-19 02:54:34 +0000

    Part reverted "Clean up" commit. Player Bots fully removed until compile fix.

--- sql/All/characters_bots.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `character_npcbot` (
-  `owner` int(11) default NULL,
-  `entry` int(11) default NULL,
-  `race` tinyint(4) default NULL,
-  `class` tinyint(4) default NULL
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
